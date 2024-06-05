66e780d4a814d19e5de2d900931b4d704c1106de
EttyLehiman <etti378@gmail.com>
2024-06-06 01:52:58 +0300

    Login with token

--- Controllers/users.controller.js
+++ Controllers/users.controller.js
@@ -1,89 +1,90 @@
 const userModel = require('../Models/user.model');
+const bcrypt = require('bcrypt');
 let id = 0;
 
 const getUsers = ('', async (req, res) => {
-    try {
-      const users = await userModel.find();
-      console.log(userModel);
-      res.status(200).send(users);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error retrieving userings');
+  try {
+    const users = await userModel.find();
+    console.log(userModel);
+    res.status(200).send(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error retrieving userings');
+  }
+});
+
+const getUserId = ('', async (req, res) => {
+  try {
+    const idParams = req.params.id;
+    const user = await userModel.findById(idParams);
+    if (!user) {
+      res.status(404).send('user not found');
+      return;
+    };
+    res.send(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error retrieving userings');
+  }
+});
+
+const addUser = async (req, res) => {
+  const { name, password, email } = req.body;
+  try {
+   
+    const newUser = new userModel({
+      _id: id++,
+      name: name,
+      password: password,
+      email: email,
+    });
+    await newUser.save();
+    res.send('Data saved successfully!');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error saving user');
+  }
+};
+
+const deleteUser = ('', async (req, res) => {
+  try {
+    const idParams = req.params.id;
+    const user = await userModel.findByIdAndDelete(idParams);
+    if (!user) {
+      res.status(404).send('user not found');
+      return;
     }
-  });
-  
-  const getUserId = ('', async (req, res) => {
-    try {
-      const idParams = req.params.id;
-      const user = await userModel.findById(idParams);
-      if (!user) {
-        res.status(404).send('user not found');
-        return;
-      };
-      res.send(user);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error retrieving userings');
+    res.send('user deleted successfully!');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error deleting user');
+  }
+});
+
+const updatedUser = ('', async (req, res) => {
+  try {
+    const idParams = req.params.id;
+    const { name, password, email } = req.body;
+    const updatedUser = await userModel.findByIdAndUpdate(
+      idParams,
+      { name, password, email },
+      { new: true },
+    );
+    if (!updatedUser) {
+      res.status(404).send('user not found...');
+      return;
     }
-  });
-  
-  const addUser = ('', async (req, res) => {
-    const data = req.body;
-    console.log(data);
-    try {
-      const newUser = new userModel({
-        _id: id++,
-        name: data.name,
-        password: data.password,
-        email: data.email,
-      });
-      await newUser.save();
-      res.send('Data saved successfully!');
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error saving user');
-    }
-  });
-  
-  const deleteUser = ('', async (req, res) => {
-    try {
-      const idParams = req.params.id;
-      const user = await userModel.findByIdAndDelete(idParams);
-      if (!user) {
-        res.status(404).send('user not found');
-        return;
-      }
-      res.send('user deleted successfully!');
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error deleting user');
-    }
-  });
-  
-  const updatedUser = ('', async (req, res) => {
-    try {
-      const idParams = req.params.id;
-      const  {name, password, email} = req.body;
-      const updatedUser = await userModel.findByIdAndUpdate(
-        idParams,
-        {name, password, email},
-        {new: true},
-      );
-      if (!updatedUser) {
-        res.status(404).send('user not found...');
-        return;
-      }
-      res.status(200).send(updatedUser);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Error updating user');
-    }
-  });
-  
-  module.exports = {
-    getUsers,
-    getUserId,
-    addUser,
-    deleteUser,
-    updatedUser
-  };
+    res.status(200).send(updatedUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error updating user');
+  }
+});
+
+module.exports = {
+  getUsers,
+  getUserId,
+  addUser,
+  deleteUser,
+  updatedUser
+};
